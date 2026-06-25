60b59627f47bf6f80098c17c9fe1d06bca6aa62a
Daniel <daniel.pires@ciandt.com>
2026-06-24 23:51:52 -0300

    chore: cria diretório para organizar controllers

--- src/main/java/com/raizesnordeste/api/api/controller/AuthController.java
+++ src/main/java/com/raizesnordeste/api/api/controller/AuthController.java
@@ -1,4 +1,4 @@
-package com.raizesnordeste.api.api;
+package com.raizesnordeste.api.api.controller;
 
 import com.raizesnordeste.api.api.dto.request.LoginRequest;
 import com.raizesnordeste.api.api.dto.request.RegistroRequest;
