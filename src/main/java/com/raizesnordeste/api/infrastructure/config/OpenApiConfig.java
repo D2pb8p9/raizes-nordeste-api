bb87022827aa3df049c8ccb132da8743004977ec
Daniel <daniel.pires@ciandt.com>
2026-06-27 18:29:44 -0300

    feat: adiciona anotações do swagger em Unidade

---
+++ src/main/java/com/raizesnordeste/api/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.raizesnordeste.api.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearer-auth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Raízes do Nordeste API")
+                        .description("API de gestão de franquias de restaurante")
+                        .version("1.0.0"));
+    }
+}
