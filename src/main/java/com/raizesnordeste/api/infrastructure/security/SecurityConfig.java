d5a5119a92a9c1e0c8e53c89ef06d8cade5379d3
Daniel <daniel.pires@ciandt.com>
2026-06-28 17:57:39 -0300

    config: adiciona rotas do swagger

--- src/main/java/com/raizesnordeste/api/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/raizesnordeste/api/infrastructure/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/registro").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/unidades", "/unidades/**").hasAnyRole("GERENTE", "FRANQUEADORA")
                         .requestMatchers(HttpMethod.POST, "/unidades").hasAnyRole("GERENTE", "FRANQUEADORA")
